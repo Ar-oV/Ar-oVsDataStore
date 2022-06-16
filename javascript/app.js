2611041d132c3cfec0a55f39f5f5f24bf7e13dc9
Ar-oV <klintbarton151295@gmail.com>
2022-06-16 21:57:03 +0300

    create grid lern

---
+++ javascript/app.js
@@ -0,0 +1,43 @@
+// 1 переменные// 
+
+const firstName = 'Artem'
+const lastName = 'Popov'
+//const age = 26
+const isProgrammer = true 
+
+const _ = 'private'
+const $ = 'some value'
+
+// const if = 'mkef' // err
+const withNum5 = '5'
+// const 5withNum = 5 // err
+
+// 2 мутирование
+// console.log('Имя человека ' + firstName + ', а возраст человека ' + age)
+//alert('Имя человека ' + firstName + ', а возраст человека ' + age)
+
+//const lastName = prompt('Введите фамилию')
+//alert(firstName + ' ' + lastName)
+
+// 3 Операторы
+let currentYear = 2022
+const birthYear = 1995
+
+//const age = currentYear - birthYear
+const a = 10
+const b = 5
+
+let c = 32
+//c = c + a 
+c += a
+c -= a
+c *= a
+c /= a
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(++currentYear)
+console.log(--currentYear)
+console.log(c)

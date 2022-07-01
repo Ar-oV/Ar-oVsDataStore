9a3a26f5496c1bf24ebffea36aebef77ed8fdabb
Ar-oV <klintbarton151295@gmail.com>
2022-07-01 07:35:21 +0300

    create icon links

--- javascript_practic/script.js
+++ javascript_practic/script.js
@@ -3,6 +3,7 @@ const footer = document.querySelector('.footer')
 const button = document.querySelector('.button__btn')
 const body = document.querySelector('body')
 const html = document.querySelector('html')
+const border = document.querySelector('.link__btn')
 
 button.onclick = () => {
     if (button.style.backgroundColor === 'gray') {
@@ -10,12 +11,18 @@ button.onclick = () => {
         footer.style.backgroundColor = '#222'
         header.style.backgroundColor = '#222'
         body.style.backgroundColor = '#333'
+        body.style.color = '#fff'
         html.style.backgroundColor = '#333'
+        html.style.color = '#fff'
+        border.style.border.color = 'gray'
     } else {
         button.style.backgroundColor = 'gray'
         footer.style.backgroundColor = 'gray'
         header.style.backgroundColor = 'gray'
         body.style.backgroundColor = '#fff'
+        body.style.color = '#333'
         html.style.backgroundColor = '#fff'
+        html.style.color = '#333'
+        border.style.border.color = '#333'
     } 
 }

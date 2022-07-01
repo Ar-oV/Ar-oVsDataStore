ecb57dc286c17e3711e6f22453dc9d48fcd26a1c
Ar-oV <klintbarton151295@gmail.com>
2022-07-01 22:06:28 +0300

    change icon and button theme

--- javascript_practic/script.js
+++ javascript_practic/script.js
@@ -3,7 +3,11 @@ const footer = document.querySelector('.footer')
 const button = document.querySelector('.button__btn')
 const body = document.querySelector('body')
 const html = document.querySelector('html')
-const border = document.querySelector('.link__btn')
+const border1 = document.querySelector('.link__btn_1')
+const border2 = document.querySelector('.link__btn_2')
+const border3 = document.querySelector('.link__btn_3')
+const border4 = document.querySelector('.link__btn_4')
+const btn = document.querySelector('.button__switch')
 
 button.onclick = () => {
     if (button.style.backgroundColor === 'gray') {
@@ -14,7 +18,13 @@ button.onclick = () => {
         body.style.color = '#fff'
         html.style.backgroundColor = '#333'
         html.style.color = '#fff'
-        border.style.border.color = 'gray'
+        border1.style.borderColor = 'gray'
+        border2.style.borderColor = 'gray'
+        border3.style.borderColor = 'gray'
+        border4.style.borderColor = 'gray'
+        btn.style.backgroundColor = '#fff'
+        btn.style.top = '-68px'
+
     } else {
         button.style.backgroundColor = 'gray'
         footer.style.backgroundColor = 'gray'
@@ -23,6 +33,11 @@ button.onclick = () => {
         body.style.color = '#333'
         html.style.backgroundColor = '#fff'
         html.style.color = '#333'
-        border.style.border.color = '#333'
+        border1.style.borderColor = '#333'
+        border2.style.borderColor = '#333'
+        border3.style.borderColor = '#333'
+        border4.style.borderColor = '#333'
+        btn.style.backgroundColor = '#333'
+        btn.style.top = '-38px'
     } 
 }

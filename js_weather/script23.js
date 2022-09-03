e6ec9d172dcc9c1ae1a8142062cedb8a5c5c6825
Ar-oV <klintbarton151295@gmail.com>
2022-09-03 10:00:26 +0300

    connected api

--- js_weather/script23.js
+++ js_weather/script23.js
@@ -1,9 +1,8 @@
-const link = "http://api.weatherapi.com/v1=ed0c6a436bf44027a59103947220209"
+fetch('https://api.openweathermap.org/data/2.5/weather?q=rayevskaya&appid=c38b3050eef7e19533341e2ea1e743dc')
+    .then(function (resp) { return resp.json() })
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (){
 
-const fetchData = async () => {
-    const result = await fetch(`${link}$query=London`);
-    const data = await result.json();
-
-    console.log(data);
-}
-fetchData();
+    });

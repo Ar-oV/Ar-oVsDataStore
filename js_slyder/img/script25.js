2a92cf557d5327bbe70afb28b02a5bfcb887158a
Ar-oV <klintbarton151295@gmail.com>
2022-09-29 07:56:41 +0300

    slider refinement

--- js_slyder/img/script25.js
+++ js_slyder/img/script25.js
@@ -1,6 +1,7 @@
 const CLIENT_ID = 'm_GwHyAP4-AIfdJnXK0SnwLiwdaZzYjSnq8ukDj4ra4';
 const slider = document.getElementById('slider');
 let state = [];
+let currentSlide;
 const fetchPhotos = async() => {
     try {
         const url = `https://api.unsplash.com/photos/random?client_id=${CLIENT_ID}&count=4`;
@@ -8,6 +9,7 @@ const fetchPhotos = async() => {
     const data = await response.json();
     if(response.ok && data.length) {
         state = data;
+        currentSlide = data[0].id
         setPhotos();
     };
     } catch (err) {
@@ -18,16 +20,25 @@ const fetchPhotos = async() => {
 };
 const renderItem = () => {
     return state
-        .map(({ urls: { regular }, user: { name} }) => {
-            return `<div class="slide" style="background-image: url(${regular})">
+        .map(({ urls: { regular }, user: { name}, id }) => {
+            const inActive = currentSlide === id ? 'active' : ''
+            return `<div class="slide ${inActive}" style="background-image: url(${regular})">
                         <div class="slide-text" >
                             <span>photo by</span>
-                            ${name}
+                            <div class="name">${name}</div>
                         </div>
                     </div>`
         }).join("");
 };
+const handleClick = (currentTarget) => {
+    console.log(currentTarget);
+};
 const setPhotos = () => {
     slider.innerHTML = renderItem();
+    const slides = document.querySelectorAll('.slide');
+    slides.forEach((slide) =>{
+        slide.addEventListener("click", handleClick);
+    });
+    console.log(slides);
 };
 fetchPhotos();

079aaa0fbefc016bd665102cda58ddf9024af42a
Ar-oV <klintbarton151295@gmail.com>
2022-10-05 22:20:53 +0300

    connected data from the API to the course screens

--- js_currency/script26.js
+++ js_currency/script26.js
@@ -1,14 +1,26 @@
-// fetch('https://www.cbr-xml-daily.ru/daily_json.js').then(function (result) {
-//     return result.json()
-// }).then(function (data) {
-//     console.log(data);
-// })
+const rates = {};
+const usd = document.querySelector('[data-value="USD"]');
+const eur = document.querySelector('[data-value="EUR"]');
+const gbp = document.querySelector('[data-value="GBP"]');
 getCurrencies();
 async function getCurrencies () {
     const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
     const data = await response.json();
     const result = await data;
-    console.log(result);
-    console.log(result.Valute.USD.Value);
+    rates.USD = result.Valute.USD;
+    rates.EUR = result.Valute.EUR;
+    rates.GBP = result.Valute.GBP;
+    console.log(rates);
+    usd.textContent = rates.USD.Value.toFixed(2);
+    eur.textContent = rates.EUR.Value.toFixed(2);
+    gbp.textContent = rates.GBP.Value.toFixed(2);
+    if(rates.USD.Value > rates.USD.Previos) {
+        usd.classList.add('green');
+    } else {usd.classList.add('red')};
+    if(rates.EUR.Value > rates.EUR.Previos) {
+        eur.classList.add('green');
+    } else {eur.classList.add('red')};
+    if(rates.GBP.Value > rates.GBP.Previos) {
+        gbp.classList.add('green');
+    } else {gbp.classList.add('red')};
 }
-let usd = document.querySelector('.usd')

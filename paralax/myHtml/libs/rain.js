2246ebee960d63bf83e5af80bc27919f484ab71e
Ar-oV <klintbarton151295@gmail.com>
2023-07-01 16:34:40 +0300

    complite parallax effects lern

--- paralax/myHtml/libs/rain.js
+++ paralax/myHtml/libs/rain.js
@@ -42,8 +42,8 @@ let rainArray = [];
 for (let i = 0; i < 140; i++) {
 	let rainXLocation = Math.floor(Math.random() * window.innerWidth) + 1;
 	let rainYLocation = Math.random() * -500;
-	let randomRainHeight = randomNum(10, 2);
-	let randomSpeed = randomNum(20, .2);
+	let randomRainHeight = randomNum(20, 5);
+	let randomSpeed = randomNum(20, .9);
 	let randomOpacity = Math.random() * .55;
 	rainArray.push(new RainDrops(rainXLocation, rainYLocation, randomRainHeight, randomSpeed, randomOpacity));
 }
